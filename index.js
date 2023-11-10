6d8dfd35672d33492e795c1475120585e25151dc
hadil <hihijazi@gmail.com>
2023-11-10 06:52:03 -0500

    save complete lab

---
+++ index.js
@@ -0,0 +1,15 @@
+function divide() {
+    const divideBy100 = () => 2000 / 100;
+    return 2000 / 100;
+  }
+function square() {
+    const square = (number) => number * number;
+    let x = 2;
+    return x * 2;
+}
+function add() {
+    const add = (number) => number + number;
+    let a = 3
+    let b = 4
+    return a + b;
+}
